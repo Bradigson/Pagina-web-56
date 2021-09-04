69c15cb3cb1f3d336c0b80df6a6644359c053221
Bradigson <71954800+Bradigson@users.noreply.github.com>
2021-09-03 23:33:48 -0400

    DONE!

--- js/app.js
+++ js/app.js
@@ -8,3 +8,34 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+
+function startTime(){
+    var today = new Date(), hours = today.getHours(), minutes = today.getMinutes(),
+    segundos = today.getSeconds();
+
+    hours = (hours == 0) ? 12 : hours;
+    hours = (hours > 12) ? hours - 12 : hours;
+
+    hours = checkTime(hours);
+    minutes = checkTime(minutes);
+
+    if(segundos < 10){
+        segundos = "0" + segundos;
+    }
+
+    var hr = document.querySelector(".time").innerHTML = hours +":"+ minutes +":"+ segundos;
+
+    var time = setTimeout(
+        function(){
+            startTime();
+        },500
+    );
+}
+function checkTime(e){
+    if(e < 10){
+        e = "0" + e;
+    }
+    return e;
+}
